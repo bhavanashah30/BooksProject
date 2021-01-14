5e7820fc474647ef5eab4eed62c0083f7542b4c1
bhavana.shah <bhavana30793@gmail.com>
2021-01-13 23:32:18 -0500

    add primary key id to details table.

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -14,15 +14,15 @@ class Detail extends Model
 
     protected $table = "Details";
 
-    protected $primaryKey = 'book_id';
+    protected $primaryKey = 'id';
 
-    protected $guarded = ['book_id'];
+    protected $guarded = ['id'];
 
     /**
      * @return BelongsTo
      */
     public function book(): BelongsTo
     {
-        $this->belongsTo(Book::class, 'book_id', 'id');
+        return $this->belongsTo(Book::class, 'book_id', 'id');
     }
 }
